afde4b66198718474a6ef7ec97342688eaf9ceac
Basil <vasya.polyakov.80@mail.ru>
2025-02-17 10:36:49 +0800

    обнова

--- sharp2/sharp2/sharp2/Program.cs
+++ sharp2/sharp2/sharp2/Program.cs
@@ -1,10 +1,8 @@
-﻿ 
+﻿using System;
+using System.Collections.Generic;
+
 namespace GradesPrototype
 {
-    // Перечисление для предметов
-     
-
-    // Структура для хранения оценки
     struct Grade
     {
         public string Subject;
@@ -18,70 +16,83 @@ namespace GradesPrototype
         {
             List<Grade> grades = new List<Grade>();
 
-            // Добавляем оценки
-            AddGrade(grades, "Math", 85, new DateTime(2023, 10, 1));
-            AddGrade(grades, "Physics", 92, new DateTime(2023, 10, 2));
-            AddGrade(grades, "Chemistry", 78, new DateTime(2023, 10, 3));
-             
-            // Выводим все оценки
-            Console.WriteLine("Все оценки:");
-            PrintGrades(grades);
-
-            // Удаляем оценку по математике
-            RemoveGradeBySubject(grades, "Math");
-            Console.WriteLine("\nОценки после удаления оценки по математике:");
-            PrintGrades(grades);
-
-            // Ищем оценки по физике
-            List<Grade> physicsGrades = FindGradesBySubject(grades, "Physics");
-            Console.WriteLine("\nОценки по физике:");
-            PrintGrades(physicsGrades);
-        }
-
-        // Метод для добавления оценки
-        static void AddGrade(List<Grade> grades,string subject, int score, DateTime date)
-        {
-            Grade grade = new Grade
+            while (true)
             {
-                Subject = subject,
-                Score = score,
-                Date = date
-            };
-            grades.Add(grade);
-        }
+                Console.WriteLine("\nВыберите действие:");
+                Console.WriteLine("1. Добавить оценку");
+                Console.WriteLine("2. Удалить оценку по предмету");
+                Console.WriteLine("3. Найти оценки по предмету");
+                Console.WriteLine("4. Вывести все оценки");
+                Console.WriteLine("5. Выход");
 
-        // Метод для удаления оценки по предмету
-        static void RemoveGradeBySubject(List<Grade> grades, string subject)
-        {
-            for (int i = 0; i < grades.Count; i++)
-            {
-                if (grades[i].Subject == subject)
+                string choice = Console.ReadLine();
+
+                switch (choice)
                 {
-                    grades.RemoveAt(i);
-                }
-            }
-        }
+                    case "1":
+                        Console.Write("Введите предмет: ");
+                        string subject = Console.ReadLine();
+                        Console.Write("Введите оценку: ");
+                        int score = int.Parse(Console.ReadLine());
+                        Console.Write("Введите дату (гггг-мм-дд): ");
+                        DateTime date = DateTime.Parse(Console.ReadLine());
 
-        // Метод для поиска оценок по предмету
-        static List<Grade> FindGradesBySubject(List<Grade> grades, string subject)
-        {
-            List<Grade> result = new List<Grade>();
-            foreach (var grade in grades)
-            {
-                if (grade.Subject == subject)
-                {
-                    result.Add(grade);
-                }
-            }
-            return result;
-        }
+                        Grade grade = new Grade
+                        {
+                            Subject = subject,
+                            Score = score,
+                            Date = date
+                        };
+                        grades.Add(grade);
+                        break;
 
-        // Метод для вывода оценок
-        static void PrintGrades(List<Grade> grades)
-        {
-            foreach (var grade in grades)
-            {
-                Console.WriteLine($"Предмет: {grade.Subject}, Оценка: {grade.Score}, Дата: {grade.Date.ToShortDateString()}");
+                    case "2":
+                        Console.Write("Введите предмет для удаления оценки: ");
+                        string subjectToRemove = Console.ReadLine();
+                        for (int i = 0; i < grades.Count; i++)
+                        {
+                            if (grades[i].Subject == subjectToRemove)
+                            {
+                                grades.RemoveAt(i);
+                                break;
+                            }
+                        }
+                        break;
+
+                    case "3":
+                        Console.Write("Введите предмет для поиска оценок: ");
+                        string subjectToFind = Console.ReadLine();
+                        List<Grade> foundGrades = new List<Grade>();
+                        foreach (var g in grades)
+                        {
+                            if (g.Subject == subjectToFind)
+                            {
+                                foundGrades.Add(g);
+                            }
+                        }
+
+                        Console.WriteLine("Найденные оценки:");
+                        foreach (var g in foundGrades)
+                        {
+                            Console.WriteLine($"Предмет: {g.Subject}, Оценка: {g.Score}, Дата: {g.Date.ToShortDateString()}");
+                        }
+                        break;
+
+                    case "4":
+                        Console.WriteLine("Все оценки:");
+                        foreach (var g in grades)
+                        {
+                            Console.WriteLine($"Предмет: {g.Subject}, Оценка: {g.Score}, Дата: {g.Date.ToShortDateString()}");
+                        }
+                        break;
+
+                    case "5":
+                        return;
+
+                    default:
+                        Console.WriteLine("Неверный выбор. Попробуйте снова.");
+                        break;
+                }
             }
         }
     }
